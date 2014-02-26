f1c61fdcf8049d203b8b1a2d126bcad50a16fc95
Che <Kaneida@users.noreply.github.com>
2014-02-26 00:25:29 +0000

    Update custom_functions.php

--- custom_functions.php
+++ custom_functions.php
@@ -17,3 +17,30 @@
 		}
 	
 START REMOVING or ADDING NEW ACTIONS BELOW THIS LINE */
+
+add_filter('bizz_car_seats', 'custom_car_seats');
+function custom_car_seats() {
+	$seats = array(
+		array('name' => '1', 'value' => '1'),
+		array('name' => '2', 'value' => '2'),
+		array('name' => '3', 'value' => '3'),
+		array('name' => '4', 'value' => '4'),
+		array('name' => '5', 'value' => '5'),
+		array('name' => '6', 'value' => '6'),
+		array('name' => '7', 'value' => '7'),
+		array('name' => '8', 'value' => '8'),
+		array('name' => '9', 'value' => '9'),
+		array('name' => '10', 'value' => '10'),
+		array('name' => '11', 'value' => '11'),
+		array('name' => '12', 'value' => '12'),
+		array('name' => '13', 'value' => '13'),
+		array('name' => '14', 'value' => '14'),
+		array('name' => '15', 'value' => '15'),
+		array('name' => '16', 'value' => '16'),
+		array('name' => '17', 'value' => '17'),
+	);
+ 
+	return $seats;
+}
+
+
