928033c0daa5d5448caa77000dd6febedec7dc86
Che <Kaneida@users.noreply.github.com>
2014-02-26 00:23:13 +0000

    Create custom_functions.php

---
+++ custom_functions.php
@@ -0,0 +1,19 @@
+<?php 
+
+/*  BizzThemes Custom Actions
+
+    *** Explore all available theme actions in lib_theme/theme_hooks.php
+	*** Explore all available framework actions in lib_frame/frame_hooks.php
+	
+	*** Working ACTION example:
+	    add_action( 'bizz_head_grid', 'bizz_head_grid_test' );
+	    function bizz_head_grid_test() {
+			echo 'test';
+		}
+	*** Working FILTER example:
+		add_filter('widget_title', 'my_widget_title');
+		function my_widget_title($title) {
+			return '<h6>' . $title . '</h6>';
+		}
+	
+START REMOVING or ADDING NEW ACTIONS BELOW THIS LINE */
